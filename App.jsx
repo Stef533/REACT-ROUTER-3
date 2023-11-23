ba4c85b2f43c2813410c9381f8e1258c01d01def
Stefan Petrea <137123241+Stef533@users.noreply.github.com>
2023-11-23 18:21:21 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { Shop } from "./HomePage";
+import { Login } from "./Login";
+import { OurStory } from "./OurStory";
+import {Routes} from 'react-router-dom'
+import {Route} from 'react-router-dom'
+
+export function App(){
+  return(
+    <Routes>
+      <Route path="shop" element={<Shop />} />
+      <Route path="login" element={<Login />} />
+      <Route path="ourstory" element={<OurStory />} />
+    </Routes>
+)
+}
